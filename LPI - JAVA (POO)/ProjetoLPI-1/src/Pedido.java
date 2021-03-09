1ec218df0524f51d5b16968b4e4cb69b8dfa162b
DanubiaM <dubamacedoo@gmail.com>
2021-03-09 16:03:21 -0400

    Implementado novos metodos em Pedido

--- LPI - JAVA (POO)/ProjetoLPI-1/src/Pedido.java
+++ LPI - JAVA (POO)/ProjetoLPI-1/src/Pedido.java
@@ -16,58 +16,29 @@ public class Pedido {
     private Date data;
     private String nomeCliente;
     private float valorTotal;
-    /**
-     * @return the id
-     */
+    
     public int getId() {
         return id;
     }
-
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the data
-     */
     public Date getData() {
         return data;
     }
-
-    /**
-     * @param data the data to set
-     */
     public void setData(Date data) {
         this.data = data;
     }
-
-    /**
-     * @return the nomeCliente
-     */
     public String getNomeCliente() {
         return nomeCliente;
     }
-
-    /**
-     * @param nomeCliente the nomeCliente to set
-     */
-    public void setNomeCliente(String nomeCliente) {
+   public void setNomeCliente(String nomeCliente) {
         this.nomeCliente = nomeCliente;
     }
-
-    /**
-     * @return the valorTotal
-     */
     public float getValorTotal() {
         return valorTotal;
     }
-
-    /**
-     * @param valorTotal the valorTotal to set
-     */
     public void setValorTotal(float valorTotal) {
         this.valorTotal = valorTotal;
     }
