b0da7e64c8031e3119cd679ee5085f76ec191433
DanubiaM <dubamacedoo@gmail.com>
2021-03-17 18:39:05 -0400

    criado classe Pessoa e iniciado implementação dos metodos

---
+++ LPI - JAVA (POO)/7- Relacionamento entre Classes/RelacionamenoEntreClasses/src/Livraria/Pessoa.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Livraria;
+
+/**
+ *
+ * @author danub
+ */
+public class Pessoa {
+
+    private String getNome() {
+        return nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private int getIdade() {
+        return idade;
+    }
+
+    private void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    private String getSexo() {
+        return sexo;
+    }
+
+    private void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    private String nome;
+    private int idade;
+    private String sexo;
+    
+    
+}
