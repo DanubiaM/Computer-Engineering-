9cf84fd3ab3fa5b508b03f6f6d7e67108ffe63f0
DanubiaM <dubamacedoo@gmail.com>
2021-03-18 11:19:32 -0400

    feat: criar classe Funcionario

---
+++ LPI - JAVA (POO)/8- Herança/Herança/src/ExemploPessoa/Funcionario.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExemploPessoa;
+
+/**
+ *
+ * @author danub
+ */
+public class Funcionario extends Pessoa{
+
+    public String setor;
+    public boolean trabalhando;
+    
+    public void mudarTrabalgo(String newSetor){
+        this.setor = newSetor;
+    }
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public boolean isTrabalhando() {
+        return trabalhando;
+    }
+
+    public void setTrabalhando(boolean trabalhando) {
+        this.trabalhando = trabalhando;
+    }
+   
+    
+    
+}
