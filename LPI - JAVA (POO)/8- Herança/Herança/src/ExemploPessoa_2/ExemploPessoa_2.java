6f70494025cbf4b97909fc9a30439c2f2076aa35
DanubiaM <dubamacedoo@gmail.com>
2021-03-18 19:40:08 -0400

    feat:criar testes

--- LPI - JAVA (POO)/8- Herança/Herança/src/ExemploPessoa_2/ExemploPessoa_2.java
+++ LPI - JAVA (POO)/8- Herança/Herança/src/ExemploPessoa_2/ExemploPessoa_2.java
@@ -5,7 +5,27 @@
 package ExemploPessoa_2;
 public class ExemploPessoa_2 {
     public static void main (String[] args){
-        
+
+    Visitante t1 = new Visitante();
+    t1.setNome("Jose");
+    t1.setIdade(23);
+    t1.setSexo("M");
+    System.out.println("Nome:"+t1.getNome()+"Idade "+t1.getIdade()+ "Sexo "+t1.getSexo());
+    
+    
+    Aluno a1 = new Aluno();
+    a1.setNome("Tie");
+    a1.setIdade(26);
+    a1.setSexo("F");
+    a1.pagarMesalidade();
+    
+    Bolsista b1 = new Bolsista();
+    b1.setNome("She");
+    b1.setIdade(12);
+    b1.setSexo("M");
+    b1.pagarMesalidade();
+    
+    
     
     }
 }
