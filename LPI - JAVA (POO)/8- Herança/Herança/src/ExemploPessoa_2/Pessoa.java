9bc96afeaad5de66e1bce8230285f5c3522190cd
DanubiaM <dubamacedoo@gmail.com>
2021-03-18 17:04:12 -0400

    feat: criar classe abstrata pessoa

---
+++ LPI - JAVA (POO)/8- Herança/Herança/src/ExemploPessoa_2/Pessoa.java
@@ -0,0 +1,41 @@
+/*
+ * Conceitos visto aqui> Classe abstrada
+ * 
+ *
+ */
+package ExemploPessoa_2;
+
+public abstract class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+    
+    public void fazerAniversario(){
+        this.idade++;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+       
+        
+}
