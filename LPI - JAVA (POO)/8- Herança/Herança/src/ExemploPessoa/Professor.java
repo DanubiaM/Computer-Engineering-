7ae7cbe72bcc932f5d9c8d2b03d3925946e9f95f
DanubiaM <dubamacedoo@gmail.com>
2021-03-18 11:25:35 -0400

    feat:criar classe Professor

---
+++ LPI - JAVA (POO)/8- Herança/Herança/src/ExemploPessoa/Professor.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExemploPessoa;
+
+/**
+ *
+ * @author danub
+ */
+public class Professor {
+
+    public String especialidade;
+    public float salario;
+    
+    public void receberAumento(float valor){
+       this.salario +=valor;
+    }
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+    
+    
+    
+}
