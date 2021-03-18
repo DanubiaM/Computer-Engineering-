4fe9d47841d3e5a1f88e289da05ebcac94d8b695
DanubiaM <dubamacedoo@gmail.com>
2021-03-18 16:57:06 -0400

    doc: Criar documentacao de exemplopessoa_2

---
+++ LPI - JAVA (POO)/8- Herança/Herança/src/ExemploPessoa_2/ExemploPessoa_2.java
@@ -0,0 +1,11 @@
+/*
+    *Os conceitos visto aqui seram mais avançados do que o da packges "ExemploPessoa"
+   
+ */
+package ExemploPessoa_2;
+public class ExemploPessoa_2 {
+    public static void main (String[] args){
+        
+    
+    }
+}
