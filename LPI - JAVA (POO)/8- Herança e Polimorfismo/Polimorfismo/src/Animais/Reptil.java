d6cd274ff5456ecd70d8d432489683b6962ad1eb
DanubiaM <dubamacedoo@gmail.com>
2021-03-22 11:32:39 -0400

    feat: criar classe Reptil

---
+++ LPI - JAVA (POO)/8- Herança e Polimorfismo/Polimorfismo/src/Animais/Reptil.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Animais;
+
+/**
+ *
+ * @author danub
+ */
+public class Reptil extends Animal{
+    private String corEscama;
+    
+    @Override
+    public void locomover(){
+        System.out.println("Rastejando");
+    }
+    
+    @Override 
+    public void alimentar(){
+        System.out.println("Comendo vegetais :3");
+    }
+    
+    @Override 
+    public void emitirSom(){
+        System.out.println("Som de um reptil");
+    }
+}
