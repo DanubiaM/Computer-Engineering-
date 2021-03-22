0d855fad33c19965671dd6cc7be27e89a4ef501a
DanubiaM <dubamacedoo@gmail.com>
2021-03-22 12:06:30 -0400

    test: adicionar main e fazer teste

---
+++ LPI - JAVA (POO)/8- Herança e Polimorfismo/Polimorfismo/src/Animais/TestandoAnimais.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Animais;
+
+public class TestandoAnimais {
+    public static void main(String[] args){
+        Mamifero  m = new Mamifero();
+        m.setPeso(10);
+        m.setIdade(4);
+        m.setMembros(4);
+        m.setCorPelo("Branco");
+        m.alimentar();
+        m.emitirSom();
+        m.locomover();
+        
+        Reptil p = new Reptil();
+        p.setPeso(3.5f);
+        p.setIdade(2);
+        p.setMembros(4);
+        p.setCorEscama("Verde");
+        p.alimentar();
+        p.emitirSom();
+        p.locomover();
+        
+        Peixe px = new Peixe();        
+        px.setPeso(0.5f);
+        px.setIdade(1);
+        px.setMembros(0);
+        px.alimentar();
+        px.emitirSom();
+        px.locomover();
+        px.soltarBolha();
+
+        Ave a = new Ave();
+        a.setPeso(1.5f);
+        a.setIdade(2);
+        a.setMembros(4);
+        a.alimentar();
+        a.emitirSom();
+        a.locomover();
+        a.fazerNinho();
+    }
+}
