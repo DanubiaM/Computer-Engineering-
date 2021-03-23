6115a488eba7a83ea5de39af375b0282faf906f2
DanubiaM <dubamacedoo@gmail.com>
2021-03-23 17:32:18 -0400

    doc: criar packge Interface

---
+++ LPI - JAVA (POO)/8- Herança e Polimorfismo/teste/src/teste.java
@@ -0,0 +1,9 @@
+package	br.com.caelum.contas.main;
+import	br.com.caelum.javafx.api.main.SistemaBancario;
+
+public class teste{
+	public	static	void	main(String[]	args) {
+									SistemaBancario.mostraTela(false);
+									//	TelaDeContas.main(args);
+					}
+}
