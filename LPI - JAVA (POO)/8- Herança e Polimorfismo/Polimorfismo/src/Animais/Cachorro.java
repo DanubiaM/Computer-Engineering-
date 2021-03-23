2fa503f9e76c6315057b107c195f548a722a6a97
DanubiaM <dubamacedoo@gmail.com>
2021-03-22 20:19:01 -0400

    feat: criar classe Cachorro

---
+++ LPI - JAVA (POO)/8- Herança e Polimorfismo/Polimorfismo/src/Animais/Cachorro.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Animais;
+
+/**
+ *
+ * @author danub
+ */
+public class Cachorro extends Mamifero{
+    
+    public void enterrarOsso(){
+        System.out.println("Enterrando osso");
+    }
+    public void abanarRabo(){
+        System.out.println("Abanando rabo");
+    }
+}
