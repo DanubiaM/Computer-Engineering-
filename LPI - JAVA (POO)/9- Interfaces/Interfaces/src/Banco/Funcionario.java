61dfc7ac564b785b7d15cce2c324fc8608615e25
DanubiaM <dubamacedoo@gmail.com>
2021-03-23 17:38:43 -0400

    feat: criar classe funcionario e implementar

---
+++ LPI - JAVA (POO)/9- Interfaces/Interfaces/src/Banco/Funcionario.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Banco;
+
+/**
+ *
+ * @author danub
+ */
+public class Funcionario {
+    private String nome;
+    private int idade;
+    private String cpf;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+   
+}
