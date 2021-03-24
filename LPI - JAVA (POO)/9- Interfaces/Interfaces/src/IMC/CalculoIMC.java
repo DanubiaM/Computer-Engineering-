06f355af3038456a81f2a62c9c11a255b6b64801
DanubiaM <dubamacedoo@gmail.com>
2021-03-24 19:50:12 -0400

    feat: criar main

---
+++ LPI - JAVA (POO)/9- Interfaces/Interfaces/src/IMC/CalculoIMC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IMC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danub
+ */
+public class CalculoIMC {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
