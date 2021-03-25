c610010cbe762817c4095885719b7c9ebc8afc38
DanubiaM <dubamacedoo@gmail.com>
2021-03-25 11:13:55 -0400

    feat: adicionar metodos static em main

--- LPI - JAVA (POO)/9- Interfaces/Interfaces/src/IMC/CalculoIMC.java
+++ LPI - JAVA (POO)/9- Interfaces/Interfaces/src/IMC/CalculoIMC.java
@@ -26,12 +26,48 @@ public class CalculoIMC {
            System.out.println("[0] SAIR");
            opcao = scanf.nextInt();
            if(opcao == 1){
-
+               calcularIMCHomem();
            }else if (opcao == 2){
-               
+               calcularIMCMulher();
            }            
         }while(opcao != 0);
         
     }
+    private static void calcularIMCHomem(){
+        Homem homem = new Homem();
+        fillDadosPessoa(homem);
+        imprimirDadosIMC(homem);
+    }
+    private static void calcularIMCMulher(){
+        Mulher mulher = new Mulher();
+        fillDadosPessoa(mulher);
+        imprimirDadosIMC(mulher);
+    }
+    private static void fillDadosPessoa(Pessoa pessoa){
+        Scanner scanf = new Scanner(System.in);
+        System.out.println("Informe a altura: ");
+        pessoa.setAltura(scanf.nextFloat());
+        System.out.println("Informe o peso: ");
+        pessoa.setPeso(scanf.nextFloat());
+    }
     
+    private static void imprimirDadosIMC(IAptoCalcularIMC apto){
+        System.out.println("DADOS DO IMC");
+        System.out.println("-=-=-=-=-=-=-=-");
+        apto.calcularIMC();
+        Pessoa pessoa = (Pessoa)apto; //conversao do apto para pessoa
+        if( pessoa instanceof Homem){
+            System.out.println("A pessoa é um homem."); // USO DE CAST
+            System.out.println("Peso"+ Float.toString(((Homem)apto).getPeso())); //link https://www.guj.com.br/t/cast/90744/5 para entender melhor!
+            System.out.println("Altura"+ Float.toString(((Homem)apto).getAltura()));
+            System.out.println("IMC"+ Float.toString(((Homem)apto).getImc()));
+        } 
+        if(pessoa instanceof Mulher){
+            System.out.println("A pessoa é uma mulher.");
+            System.out.println("Peso"+ Float.toString(((Mulher)apto).getPeso()));
+            System.out.println("Altura"+ Float.toString(((Mulher)apto).getAltura()));
+            System.out.println("IMC"+ Float.toString(((Mulher)apto).getImc()));
+        }
+
+    }
 }
