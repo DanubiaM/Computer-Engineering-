0ea1badf6730076a164599b6e48237aab93fb272
DanubiaM <dubamacedoo@gmail.com>
2021-03-24 20:36:27 -0400

    feat: criar e implementar classe Homem

---
+++ LPI - JAVA (POO)/9- Interfaces/Interfaces/src/IMC/Homem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IMC;
+
+/**
+ *
+ * @author danub
+ */
+public class Homem extends Pessoa implements IAptoCalcularIMC{
+    
+    public void calcularIMC(){
+        float imc = 0;
+        float peso = this.getPeso();
+        float altura = this.getAltura();
+        imc = peso / (altura*altura);
+        this.setImc(imc);
+    }
+}
