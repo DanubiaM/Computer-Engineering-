db994ab86f9d4838fd89d7974b32414273e8eb18
DanubiaM <dubamacedoo@gmail.com>
2021-03-30 19:54:37 -0400

    feat: criar classe Pessoa e implementar

---
+++ LPI - JAVA (POO)/ProjetoFinal/ProjetoFinal/src/projetofinal/Pessoa.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetofinal;
+
+/**
+ *
+ * @author danub
+ */
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected String sexo;
+    protected int experiencia;
+ 
+    protected void ganharExp(int exp){
+        this.experiencia+=exp;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }    
+    
+}
