8d66652af85170f0cb008526ee7243489df5bb4d
DanubiaM <dubamacedoo@gmail.com>
2021-03-30 19:58:17 -0400

    feat: criar e implemetar classe Gafanhoto

---
+++ LPI - JAVA (POO)/ProjetoFinal/ProjetoFinal/src/projetofinal/Gafanhoto.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetofinal;
+
+/**
+ *
+ * @author danub
+ */
+public class Gafanhoto {
+    private String login;
+    private int totAssistido;
+    
+    public void viuMaisUm(){
+        this.totAssistido++;
+    }
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+   
+    
+    
+}
