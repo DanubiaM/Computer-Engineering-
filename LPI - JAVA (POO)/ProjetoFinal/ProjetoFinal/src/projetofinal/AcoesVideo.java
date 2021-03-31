72d762d5a9f456b951d657f709ef139a93226b4d
DanubiaM <dubamacedoo@gmail.com>
2021-03-30 20:04:56 -0400

    feat: implementar interface AcoesVideo

---
+++ LPI - JAVA (POO)/ProjetoFinal/ProjetoFinal/src/projetofinal/AcoesVideo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetofinal;
+
+/**
+ *
+ * @author danub
+ */
+public interface AcoesVideo {
+    public void play();
+    public void pause();
+    public void like();
+}
