ff2857efc36e00b43772e0d41e68bc245c15f2cd
DanubiaM <dubamacedoo@gmail.com>
2021-03-30 20:56:46 -0400

    refactor: alterado classe pessoa para abstrata

--- LPI - JAVA (POO)/ProjetoFinal/ProjetoFinal/src/projetofinal/Pessoa.java
+++ LPI - JAVA (POO)/ProjetoFinal/ProjetoFinal/src/projetofinal/Pessoa.java
@@ -9,7 +9,7 @@ package projetofinal;
  *
  * @author danub
  */
-public class Pessoa {
+public abstract class Pessoa {
     protected String nome;
     protected int idade;
     protected String sexo;
