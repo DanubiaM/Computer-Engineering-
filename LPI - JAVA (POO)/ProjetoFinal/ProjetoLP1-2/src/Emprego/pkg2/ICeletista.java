bb456fa4ae8fb3ae4885990f5926674b2ed03c5b
DanubiaM <dubamacedoo@gmail.com>
2021-04-01 16:59:32 -0400

    feat: implementar interface ICeletista

---
+++ LPI - JAVA (POO)/ProjetoFinal/ProjetoLP1-2/src/Emprego/pkg2/ICeletista.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Emprego.pkg2;
+
+/**
+ *
+ * @author danub
+ */
+public interface ICeletista {
+    public void informeRAIS();
+}
