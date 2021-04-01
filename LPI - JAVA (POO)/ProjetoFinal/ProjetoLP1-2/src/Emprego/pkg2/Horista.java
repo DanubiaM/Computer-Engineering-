718524548343513c79c8a7ffd6f691156f9cba3e
DanubiaM <dubamacedoo@gmail.com>
2021-04-01 16:56:57 -0400

    feat: criar e implementar classe Horista

---
+++ LPI - JAVA (POO)/ProjetoFinal/ProjetoLP1-2/src/Emprego/pkg2/Horista.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Emprego.pkg2;
+
+/**
+ *
+ * @author danub
+ */
+public class Horista extends Empregado{
+    private float valorHora;
+
+    public float getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(float valorHora) {
+        this.valorHora = valorHora;
+    }
+    
+}
