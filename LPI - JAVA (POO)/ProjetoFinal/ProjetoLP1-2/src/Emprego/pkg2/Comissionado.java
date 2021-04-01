7d41ac461154f9247be94ba5fe365f36c82e97d9
DanubiaM <dubamacedoo@gmail.com>
2021-04-01 16:58:24 -0400

    feat: criar e implementar classe Comissionado

---
+++ LPI - JAVA (POO)/ProjetoFinal/ProjetoLP1-2/src/Emprego/pkg2/Comissionado.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Emprego.pkg2;
+
+/**
+ *
+ * @author danub
+ */
+public class Comissionado extends Empregado{
+    private float porcentualComissao;
+
+    public float getPorcentualComissao() {
+        return porcentualComissao;
+    }
+
+    public void setPorcentualComissao(float porcentualComissao) {
+        this.porcentualComissao = porcentualComissao;
+    }
+    
+    
+}
