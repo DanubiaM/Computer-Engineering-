932babc05f817190190a3337be5ff32275712c11
DanubiaM <dubamacedoo@gmail.com>
2021-04-01 16:50:06 -0400

    feat: criar classe Empregado e implementar

---
+++ LPI - JAVA (POO)/ProjetoFinal/ProjetoLP1-2/src/Emprego/pkg2/Empregado.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Emprego.pkg2;
+
+import java.util.Date;
+
+/**
+ *
+ * @author danub
+ */
+public abstract class Empregado {
+    private int codigo;
+    private String  nome;
+    private Date dataAdmissao;
+    private String cargo;
+    private String departamento;
+    
+    
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(Date dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+   
+    
+    
+}
