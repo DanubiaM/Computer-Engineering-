4458fe21395ecd5489f89758808d80baa6093ea9
DanubiaM <dubamacedoo@gmail.com>
2021-05-12 22:06:42 -0400

    alterado nome da pasta e criado arquivo de anotacoes

---
+++ LPII - JAVA + PostgreSQL/Statement/src/Statement/StatementTeste.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Statement;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.Statement;
+/**
+ *
+ * @author danub
+ */
+public class StatementTeste {
+    public static void main(String args[]){
+        Connection conexao = FabricaConexao.obterConexao();
+        Statement comando = null;
+        String nome = JOptionPane.showInputDialog("Forneca o nome do grupo de produto");
+        float promocao = Float.parseFloat(JOptionPane.showInputDialog("Forneca o percentual de promocao do grupo de produto"));
+        float margem = Float.parseFloat(JOptionPane.showInputDialog("Forneca o percentual de margem de lucro do grupo de produto"));
+        String sql = "INSERT INTO grupoproduto (nome, promocao, margemlucro) VALUES ";
+        sql += "('"+nome+"', "+promocao+", "+margem+")";
+        try{
+            comando = conexao.createStatement();
+            comando.executeUpdate(sql);
+            System.out.println("Inclusao realizada com sucesso!");
+        }catch(SQLException ex){
+            System.out.println("Inclusa realizada com sucesso - "+ ex.getMessage());
+        } finally {
+            try{
+                comando.close();
+                conexao.close();
+            }catch(SQLException ex){
+                System.out.println("Erro ao desconectar "+ ex.getMessage());
+            }
+        }
+        }
+
+    private void executeUpdate(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
