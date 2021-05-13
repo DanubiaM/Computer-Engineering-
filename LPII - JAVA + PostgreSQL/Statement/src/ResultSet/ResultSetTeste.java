9ab8ac176102778304709cafc00cfd1645899681
DanubiaM <dubamacedoo@gmail.com>
2021-05-13 11:13:51 -0400

    implementado a listagem dos produtos

--- LPII - JAVA + PostgreSQL/Statement/src/ResultSet/ResultSetTeste.java
+++ LPII - JAVA + PostgreSQL/Statement/src/ResultSet/ResultSetTeste.java
@@ -5,10 +5,47 @@
  */
 package ResultSet;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author danub
  */
 public class ResultSetTeste {
+    public static void main(String args[]){
+        Connection conexao = FabricaConexao.obterConexao();
+        PreparedStatement comando = null;
     
+        //listando os produtos
+       
+        try{
+            comando = conexao.prepareStatement("SELECT * FROM grupoproduto ORDER BY nome");
+            ResultSet  resultado = comando.executeQuery();
+            
+            while(resultado.next()){                
+                System.out.println("Codigo:             "+ resultado.getInt("codigo"));                   
+                System.out.println("Nome:               "+ resultado.getString("nome"));
+                System.out.println("% Promocao:         "+ resultado.getFloat("promocao"));
+                System.out.println("% Margem Lucro:     "+ resultado.getFloat("margemlucro"));
+                System.out.println("------------------------------------------------------");
+
+            }
+            resultado.close();
+        }catch(SQLException ex){
+            System.out.println("Erro ao recuperar um grupo "+ ex.toString());
+
+        }finally{
+            try{
+                comando.close();
+                conexao.close();
+            }catch(SQLException ex){
+                System.out.println("Erro ao desconectar "+ ex.toString());
+
+            }
+        }
+    }   
 }
