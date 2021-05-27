89d93da322dd4833ed17fb418d950cb8c09b862d
DanubiaM <dubamacedoo@gmail.com>
2021-05-26 20:10:51 -0400

    implementado metodo de inclusao e adicionado imports

--- LPII - JAVA + PostgreSQL/Implementacao em camadas/Implementacao_camadas/src/negocio/AlunoNegocio.java
+++ LPII - JAVA + PostgreSQL/Implementacao em camadas/Implementacao_camadas/src/negocio/AlunoNegocio.java
@@ -4,6 +4,11 @@
 
 package negocio;
 
+import java.util.List;
+import persistencia.AlunoDAO;
+import persistencia.Conexaobd
+import persistencia.PersistenciaException;
+import vo.AlunoVO;
 
 public class AlunoNegocio {
 
@@ -16,6 +21,21 @@ public class AlunoNegocio {
             throw new NegocioException("Erro ao iniciar a Persistencia - "+ ex.getMessage());
         }
 
+    }
+
+    public void inserir(AlunoVO alunoVO) throws NegocioException{
+        String mensagemErros = this.validarDados(alunoVO);
+        
+        if(!mensagemErros.isEmpty()){
+            throw new NegocioException(mensagemErros);
+        }
+        try{
+            if(alunoDAO.incluir(alunoVO) == 0){
+               throw new NegocioException("Inclusao nao realizada!"); 
+            }
+        }catch(SQLException ex){
+            throw new NegocioException("Erro ao incluir o aluno "+ ex.getMessage());
+        }
     }
 
 }
