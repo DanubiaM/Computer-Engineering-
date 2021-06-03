69dcbb93552b6aaa80713dbbc0505df5c906e0f8
DanubiaM <dubamacedoo@gmail.com>
2021-06-03 16:55:20 -0400

    Corrigido bug ]

--- LPII - JAVA + PostgreSQL/Implementacao em camadas/Implementacao_camadas/src/negocio/NegocioException.java
+++ LPII - JAVA + PostgreSQL/Implementacao em camadas/Implementacao_camadas/src/negocio/NegocioException.java
@@ -6,11 +6,11 @@ package negocio;
 
 public class NegocioException extends Exception {
     public NegocioException(){
-        super("Erro ocorrido  na camada de negocio")
+        super("Erro ocorrido  na camada de negocio");
     }
 
     public negocio Exceptio(String msg){
-        super(msg)
+        super(msg);
     }
 
 }
