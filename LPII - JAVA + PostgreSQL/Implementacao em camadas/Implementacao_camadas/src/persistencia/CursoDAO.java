23fd1ba2a61054e52e80e78b52e93f844b0d2c4a
DanubiaM <dubamacedoo@gmail.com>
2021-06-20 17:16:04 -0400

    implementado metodos de inclusao, alteracao, exclusao e buscaPorcodigo

--- LPII - JAVA + PostgreSQL/Implementacao em camadas/Implementacao_camadas/src/persistencia/CursoDAO.java
+++ LPII - JAVA + PostgreSQL/Implementacao em camadas/Implementacao_camadas/src/persistencia/CursoDAO.java
@@ -29,4 +29,62 @@ public class CursoDAO extends DAO{
 
     }
 
+    public int incluir(CursoVO cursoVO) throws PersistenciaException{
+        int retorno = 0;
+
+        try{
+            comandoIncluir.setString(1, cursoVO.getCurso());
+            comandoIncluir.setString(2, cursoVO.getDescricao());
+            retorno = comandoIncluir.executeUpdate();
+        }catch (SQLException ex){
+            throw new PersistenciaException("Erro ao incluir curso - " +ex.getMessage());
+
+        }
+
+        return retorno;
+
+    }
+
+    public int alterar(CursoVO cursoVO) throws PersistenciaException{
+        int retorno  = 0;
+
+        try{
+            comandoAlterar.setString(1, cursoVO.getCurso());
+            comandoAlterar.setString(2, cursoVO.getDescricao());
+            retorno  = comandoAlterar.executeUpdate();
+        }catch (SQLException ex){
+            throw new PersistenciaException("Erro ao alterar curso - "+ex.getMessage());
+
+        }
+        return retorno;
+    }
+
+    public int excluir( int codigo) throws PersistenciaException{
+        int retorno = 0;
+    
+        try{
+            comandoExcluir.setInt(1, codigo);
+            retorno = comandoExcluir.executeUpdate();
+        }catch (SQLException ex){
+            throw new PersistenciaException("Erro ao excluir curso - "+ex.getMessage());
+        }
+    
+        return retorno;
+    }
+
+    public CursoVO buscarPorCodigo (int codigo) throws PersistenciaException{
+        CursoVO curso = null;
+
+        try{
+            comandoBuscaCodigo.setInt(1, codigo);
+            ResultSet rs = comandoBuscaCodigo.executeQuery();
+            if(rs.next()){
+                curso = this.montaCursoVO(rs);
+            }
+
+        }catch (Exception ex){
+            throw new PersistenciaException ("Erro na seleção por codigo - "+ex.getMessage());
+        }
+        return curso;
+    }
 }
