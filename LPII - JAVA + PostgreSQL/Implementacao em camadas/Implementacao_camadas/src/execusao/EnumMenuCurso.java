52ff6b13bf0112804cdf5dda69246acc96458274
DanubiaM <dubamacedoo@gmail.com>
2021-06-21 16:42:24 -0400

    criado enum para curso

---
+++ LPII - JAVA + PostgreSQL/Implementacao em camadas/Implementacao_camadas/src/execusao/EnumMenuCurso.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package execusao;
+
+/**
+ *
+ * @author danub
+ */
+public class EnumMenuCurso {
+    IncluirCurso,
+    AlterarCurso,
+    ExcluirCurso,
+    PesqCodigo,
+    PesqNome,  
+    Sair
+}
