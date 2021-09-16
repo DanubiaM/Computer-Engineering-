b2cd8f435a91fecd85cb7e1be8a8161f8175fc02
DanubiaM <dubamacedoo@gmail.com>
2021-09-16 15:35:17 -0400

    doc refactor

---
+++ LPIII -Flutter/econ_aulas/lib/models/database.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// inicializar a instancia do firebase localmente
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+class Database {}
