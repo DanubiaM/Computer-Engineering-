75b3f1f727036bd48ecd0a0f0e819a14df08bfe9
DanubiaM <dubamacedoo@gmail.com>
2021-10-31 22:05:18 -0400

    feat: create data persistence in db

--- LPIII -Flutter/econ_aulas/lib/models/database.dart
+++ LPIII -Flutter/econ_aulas/lib/models/database.dart
@@ -1,6 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-FirebaseFirestore _firestore = FirebaseFirestore.instance;
+//FirebaseFirestore permite acesso aos dados do banco firebase
+FirebaseFirestore _firestore = FirebaseFirestore.instance; //instanciando
 CollectionReference _reference = _firestore.collection("academico");
 
-class Database {}
+class Database {
+  late String userId;
+
+  //metodo para adicionar dados no firebase
+  addStudent(String name, String socialMedia, String sexo, int age) {
+    DocumentReference documentReference =
+        _reference.doc(userId).collection('students').doc();
+
+    //String refere-se aos nomes das 'colunas' e dynamic ao tipo de dado delas
+    Map<String, dynamic> data = <String, dynamic>{
+      "name": name,
+      "socialMedia": socialMedia,
+      "sexo": sexo,
+      "age": age,
+    };
+
+    documentReference.set(data).whenComplete(() => print("Student saved!"));
+  }
+}
