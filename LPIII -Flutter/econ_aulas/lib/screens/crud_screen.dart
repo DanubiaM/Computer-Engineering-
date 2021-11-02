1eda2038196abf6887807db4565b8a52679a42c9
DanubiaM <dubamacedoo@gmail.com>
2021-11-02 11:52:20 -0400

    feat: create crud screen

---
+++ LPIII -Flutter/econ_aulas/lib/screens/crud_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CrudScreen extends StatelessWidget {
+  const CrudScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Academy system"),
+      ),
+    );
+  }
+}
