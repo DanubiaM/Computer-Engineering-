ff1a167f74dd8d33a8016019145faf36824e4af8
DanubiaM <dubamacedoo@gmail.com>
2021-11-03 17:55:32 -0400

    feat: added controllers and input field

--- LPIII -Flutter/econ_aulas/lib/screens/add_student_screen.dart
+++ LPIII -Flutter/econ_aulas/lib/screens/add_student_screen.dart
@@ -1,3 +1,4 @@
+import 'package:econ_aulas/models/database.dart';
 import 'package:flutter/material.dart';
 
 class AddStudentScreen extends StatefulWidget {
@@ -8,10 +9,49 @@ class AddStudentScreen extends StatefulWidget {
 }
 
 class _AddStudentScreenState extends State<AddStudentScreen> {
+  TextEditingController _controllerName = TextEditingController();
+  TextEditingController _controllerSocialMedia = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.limeAccent[50],
+      body: Form(
+          child: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          children: [
+            Text('Cadastro de Estudantes'),
+            TextFormField(
+              controller: _controllerName,
+              decoration: InputDecoration(
+                  labelText: "Name of Student",
+                  labelStyle: TextStyle(color: Colors.blue),
+                  hintText: 'Please, write student name'),
+            ),
+            TextFormField(
+              controller: _controllerSocialMedia,
+              decoration: InputDecoration(
+                  labelText: "Social Media",
+                  labelStyle: TextStyle(color: Colors.blue),
+                  hintText: 'Please, enter with your social media'),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Database.addStudent(
+                    name: _controllerName.text,
+                    socialMedia: _controllerSocialMedia.text);
+                //Volta para a tela anterior ao clicar no botão
+                Navigator.of(context).pop();
+              },
+              child: Text("Add"),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
