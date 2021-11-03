00711db07c8648e9ed3514887126b482caf7dbc9
DanubiaM <dubamacedoo@gmail.com>
2021-11-03 17:06:01 -0400

    feat: crate screen for add student

---
+++ LPIII -Flutter/econ_aulas/lib/screens/add_student_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddStudentScreen extends StatefulWidget {
+  const AddStudentScreen({Key? key}) : super(key: key);
+
+  @override
+  _AddStudentScreenState createState() => _AddStudentScreenState();
+}
+
+class _AddStudentScreenState extends State<AddStudentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.limeAccent[50],
+    );
+  }
+}
