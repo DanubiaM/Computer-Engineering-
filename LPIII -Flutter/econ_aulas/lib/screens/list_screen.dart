cf245ddfee8abf80250f8221a39831b64b261847
DanubiaM <dubamacedoo@gmail.com>
2021-11-03 16:27:09 -0400

    feat: implement list screen

--- LPIII -Flutter/econ_aulas/lib/screens/list_screen.dart
+++ LPIII -Flutter/econ_aulas/lib/screens/list_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:econ_aulas/models/database.dart';
 import 'package:flutter/material.dart';
 
 class ListScreen extends StatelessWidget {
@@ -5,6 +7,26 @@ class ListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder<QuerySnapshot>(
+        stream: Database.studentsList(),
+        builder: (context, snapshot) {
+          return ListView.separated(
+            //Docs pode retornar nulo, logo é adicionado !, garantido que caso nao tenha nada apareça nada mas nao deê erro
+            itemCount: snapshot.data!.docs.length,
+            separatorBuilder: (context, index) => SizedBox(
+              height: 5,
+            ),
+            itemBuilder: (context, index) {
+              String docId = snapshot.data!.docs[index].id;
+              String name = snapshot.data!.docs[index].get('name');
+              String socialMedia =
+                  snapshot.data!.docs[index].get('socialMedia');
+              return ListTile(
+                title: Text(name),
+                subtitle: Text(socialMedia),
+              );
+            },
+          );
+        });
   }
 }
